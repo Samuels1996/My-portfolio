d65e01e5fa3a6760c3ff9d40732726f094f516d2
Brian Samuels <samuels.brian9@gmail.com>
2022-05-22 13:32:51 -0400

    adding in logod for4 clickable links in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,15 @@
+import Linkedin from '../images/linkedin.png';
+import Github from '../images/github-logo.png';
+import Stackflow from '../images/stack-overflow.png'
+import { NavLink } from 'react-router-dom';
+
+
 function Footer() {
     return (
         <footer className="footer">
-                <a href="https://github.com/Samuels1996">Github</a>
-                <a href="https://www.linkedin.com/in/brian-samuels-03458b14a/">LinkedIn</a>
+                <a href="https://github.com/Samuels1996"><img src={Github} /></a>
+                <a href="https://www.linkedin.com/in/brian-samuels-03458b14a/"><img src={Linkedin} /></a>
+                <a href="https://stackoverflow.com/questions/2281087/center-a-div-in-css/"><img src={Stackflow} /></a>
         </footer>
     )
 }
